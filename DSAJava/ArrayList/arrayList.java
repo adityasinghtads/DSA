71704afb6eb64ff8eed186d6d0b08e21bb71858a
Thakur Aditya Dhanraj Singh <aditya.dhanraj@internal.utradesolutions.com>
2023-04-09 21:41:35 +0530

    Array List in Java

---
+++ DSAJava/ArrayList/arrayList.java
@@ -0,0 +1,23 @@
+package DSAJava.ArrayList;
+
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        // Array List in Java
+        // Dynamic way of storing Elements , No fixed size.
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(1);
+        for (int i = 0; i < 4; i++) {
+            al.add(i);
+        }
+        System.out.println(al);
+
+        int wipro = al.get(0);
+        System.out.println(wipro);
+        al.set(0, 2);
+        wipro = al.get(0);
+        System.out.println(wipro);
+
+    }
+}
